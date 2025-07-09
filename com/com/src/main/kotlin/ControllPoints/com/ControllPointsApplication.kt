5acb727a4f39a798b8ff57c1f52900865081c0c1
Rafael Brandao <rbs13bg13@gmail.com>
2025-07-09 18:58:54 -0300

    Criado o projeto inicial

---
+++ com/com/src/main/kotlin/ControllPoints/com/ControllPointsApplication.kt
@@ -0,0 +1,11 @@
+package ControllPoints.com
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControllPointsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ControllPointsApplication>(*args)
+}
---
+++ com/com/src/test/kotlin/ControllPoints/com/ControllPointsApplicationTests.kt
@@ -0,0 +1,13 @@
+package ControllPoints.com
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ControllPointsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
